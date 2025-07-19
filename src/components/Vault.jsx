5c6575c0ed5baa351403c04bd8019d19c3c3e8f5
raj45alok <156644544+raj45alok@users.noreply.github.com>
2025-07-19 15:31:54 +0530

    Fix: Vault.css path error for Vercel

--- src/components/Vault.jsx
+++ src/components/Vault.jsx
@@ -1,6 +1,6 @@
 // Vault.jsx
 
-import '../Styles/Vault.css';
+import '../styles/Vault.css';
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
